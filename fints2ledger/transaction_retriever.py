0456f88dec93f705a498fe387375b5f7a398ff69
Dario Ernst <dario@kanojo.de>
2021-01-23 18:51:20 +0100

    Add simple tan sending support

--- fints2ledger/transaction_retriever.py
+++ fints2ledger/transaction_retriever.py
@@ -1,14 +1,20 @@
+from fints.client import NeedTANResponse
+
 class TRetriever:
     def __init__(self, client, accountnumber):
         self.client = client
         self.accountnumber = accountnumber
 
     def get_hbci_transactions(self, start_date, end_date):
-        accounts = self.client.get_sepa_accounts()
-
-        account = self.__find_matching_account(accounts, self.accountnumber)
-
-        return self.client.get_transactions(account, start_date, end_date)
+        with self.client:
+            accounts = self.client.get_sepa_accounts()
+            account = self.__find_matching_account(accounts, self.accountnumber)
+            transactions = self.client.get_transactions(account, start_date, end_date)
+            if type(transactions) is NeedTANResponse:
+                tan = input(transactions.challenge)
+                lol = self.client.send_tan(transactions, tan)
+                transactions = self.client.get_transactions(account, start_date, end_date)
+        return transactions
 
     def __find_matching_account(self, accounts, accountnumber):
         for account in accounts:
